e01f92320803cae254aa27e3b18a1109badc168b
ansteh <stehle.andre@googlemail.com>
2017-06-26 01:06:40 +0200

    tests

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const _      = require('lodash');
 let start = new Date();
 let end = moment(start).add(10, 'days').toDate();
 let delimiter = 'days';
-let dates = DP.range(start, end, delimiter);
+//let dates = DP.range(start, end, delimiter);
 // console.log(dates);
 // console.log(DP.groupBy(dates, 'days'));
 
@@ -33,8 +33,8 @@ let getCashflow = (date, delimiter = 'month') => {
 
     let sliced = DP.slice(collection, start, end, info => info.date);
 
-    // console.log('sliced', sliced);
-
+    console.log(date, start, end, sliced);
+    
     return _
       .chain(sliced)
       .map('value')
@@ -42,23 +42,42 @@ let getCashflow = (date, delimiter = 'month') => {
       .value();
   };
 
-let todayValue = getCashflow(new Date());
-console.log('result:', todayValue);
-console.log('first month:', 10 === todayValue);
-
-let lastMonth = moment().startOf('month').subtract(10, 'day').toDate();
-console.log('last month value:', getCashflow(lastMonth), 5 === getCashflow(lastMonth));
+// let todayValue = getCashflow(new Date());
+// console.log('result:', todayValue);
+// console.log('first month:', 10 === todayValue);
+//
+// let lastMonth = moment().startOf('month').subtract(10, 'day').toDate();
+// console.log('last month value:', getCashflow(lastMonth), 5 === getCashflow(lastMonth));
 
 const testSliceEnd = (date = moment().endOf('year'), delimiter = 'month') => {
   let start = moment(_.clone(date)).startOf(delimiter);
   let end = moment(_.clone(date)).endOf(delimiter);
 
-  // console.log(start);
-  // console.log(end);
+  console.log(start);
+  console.log(end);
 
   let sliced = DP.slice(collection, start, end, info => info.date);
-  // console.log(sliced);
+  console.log(sliced);
   console.log(sliced.length === 0);
 }
 
-testSliceEnd();
+// testSliceEnd();
+
+const testDateRange = () => {
+  let start = moment().startOf('year').add(1, 'hours');
+  let end = moment().endOf('year');
+  // console.log('start', start);
+  // console.log('end', end);
+
+  let range = DP.range(start, end, 'month');
+  // console.log(range);
+  // let sliced = DP.slice(collection, start, end, info => info.date);
+  // console.log(sliced);
+  _.forEach(range, (date) => {
+    console.log(date, getCashflow(date));
+  });
+};
+
+testDateRange();
+
+console.log((new Date()).toString(), moment().toDate());
